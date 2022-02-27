e1ba5eabe93de399fb435f8f71a48fb2da627bb0
halebane <Halebane>
2022-02-27 20:47:00 +0500

    #1 Создать телеграм бота

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import telebot
+from settings import TOKEN
 
-bot = telebot.TeleBot('5217047384:AAFlJ1mQ30ET7u8GE2MqXG4A_FNx5JIZkUQ')
+bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
